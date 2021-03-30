c151aa0df5f6773ca6f0bf0b1449a91208409d2b
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-03-30 09:18:55 +0300

    fixed convertion bug

--- ex0.c
+++ ex0.c
@@ -2,26 +2,38 @@
 #include <assert.h>
 #include <stdlib.h>
 
+int charToDec(char c) {
+    int d;
+    d = (int)c;
+    if (d >= 48 && d <= 57)
+        return d - 48;
+    else
+        return d - 87;
+}
+
 int main(void) {
-    int a,b;
+    int a,b,d;
     char c;
 
     printf("Please enter the numbers base:\n");
-    scanf("%d\n", &a);
+    scanf("%d", &a);
     if (a<2 || a>16)
         printf("Invalid input base\n");
     assert(a >= 2 && a <= 16);
 
     printf("Please enter the desired base:\n");
-    scanf("%d\n", &b);
+    scanf("%d", &b);
     if (b<2 || b>16)
         printf("Invalid desired base\n");
     assert(b >= 2 && b <= 16);
     
+    getchar();
     printf("Please enter a number in base %d:\n" , a);
     while ((c = getchar()) != '\n') {
-        if (a >= 2 && a <= 10)
-            assert(atoi(&c) >= 0 && atoi(&c) < a);
+        d = charToDec(c);
+        if (d < 0 || d >= a) 
+            printf("Invalid number!\n");
+        assert(d >= 0 && d < a);
     }
     printf("end");
 
